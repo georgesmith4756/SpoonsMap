d04e07e3d80c0d4fd640a7c526f738a7828090b4
George Smith <georgesmith4756@hotmail.co.uk>
2019-09-06 09:02:06 +0100

    changes added

---
+++ script.js
@@ -0,0 +1,13 @@
+
+ var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([-2.4711, 54.5701]),
+          zoom: 6
+        })
+      });

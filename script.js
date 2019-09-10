e58017a2f40b8dadb3ae978021a1dd9bc719f883
George Smith <georgesmith4756@hotmail.co.uk>
2019-09-10 21:47:34 +0100

    added javascript functionality (retrieve pubs visited for particular user)

--- script.js
+++ script.js
@@ -12,28 +12,67 @@
         })
       });
 	  
-	  
-function retrievePubsVisited(){
-	if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","conn.php",true);
-        xmlhttp.send();
+function httpRequest(method,url,callback,headers,body){
 	
+  const xml = new XMLHttpRequest();
+  xml.open(method,url);
+  xml.onload = () => {
+  let data = xml.response;
+  callback(data);
+  }
+  for(key in headers){
+    xml.setRequestHeader(key,headers[key]);
+          }
+     body ? xml.send(body) : xml.send();
+      } 
+
+function retrievePubsVisited(){
+	
+	let method = "GET";
+	let url = "http://localhost:9000/pubs"
+	let callback = getEntries;
+	let headers = {
+		"Accept": "application/json;charset=UTF-8",
+	}
+	httpRequest(method,url,callback,headers);
 	
 	
 }
+
+function retrievePubLocation(postcode){
+  
+	let method = "GET";
+	let url = "api.postcodes.io/postcodes/" + postcode;
+	let callback = getLatLon;
+	let headers = {
+		"Accept": "application/json;charset=UTF-8",
+	}
+	httpRequest(method,url,callback,headers);
+	
+	
+}
+
+function getLatLon(data){
+  retData = JSON.parse(data);
+
+  console.log(retData.longitude);
+
+}
+
+function getEntries(data){
+  retData = JSON.parse(data);
+  
+  input = document.getElementById("enteredEmail").value;
+
+  for(i=0;i<retData.length;i++){
+  if(retData[i].username == input){
+    retrievePubLocation(retData[i].postcode)
+  }
+}
+}
+
 	  
-function addMarker(){
+function addMarker(lat,lon){
 	  
  var layer = new ol.layer.Vector({
      source: new ol.source.Vector({
